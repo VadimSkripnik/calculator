56180053683f0606b168a6a826ef82944b6fb571
Skripnik Vadim <tsermer2015@gmail.com>
2022-01-27 20:11:57 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const oneValue = document.querySelector("#oneValue"),
+    towValue = document.querySelector("#towValue"),
+    freeValue = document.querySelector("#freeValue"),
+    foreValue = document.querySelector("#foreValue");
+
+foreValue.addEventListener("click", function () {
+    let a = JSON.parse(oneValue.value);
+    let b = JSON.parse(towValue.value);
+    let operation = freeValue;
+
+    if (operation.value === "+") {
+        console.log(a + b);
+    } else if (operation.value === "-") {
+        console.log(a - b);
+    } else if (operation.value === "*") {
+        console.log(a * b);
+    } else if (operation.value === "/") {
+        console.log(a / b);
+    }
+});
